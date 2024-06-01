55c30ef174346bc3621a6591c6da56715d992362
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-06-01 11:47:53 +0200

    Create video-controller.js

---
+++ video-controller.js
@@ -0,0 +1,27 @@
+/** Windows
+wmic path win32_VideoController get AdapterRAM
+AdapterRAM
+4293918720
+
+
+wmic path win32_VideoController get driverVersion
+DriverVersion
+31.0.15.4633
+
+
+wmic path win32_VideoController get name
+Name
+NVIDIA GeForce RTX 4090 Ti
+
+// Linux
+glxinfo | grep -E -i 'device|memory|video'
+    GLX_SGI_swap_control, GLX_SGI_video_sync
+    Device: D3D12 (NVIDIA GeForce RTX 4090 Ti) (0xffffffff)
+    Video memory: 32425MB
+    Unified memory: no
+    GL_EXT_framebuffer_object, GL_EXT_framebuffer_sRGB, GL_EXT_memory_object,
+    GL_EXT_memory_object_fd, GL_EXT_packed_depth_stencil, GL_EXT_packed_float,
+    GL_EXT_gpu_program_parameters, GL_EXT_gpu_shader4, GL_EXT_memory_object,
+    GL_EXT_memory_object_fd, GL_EXT_multi_draw_arrays,
+    GL_EXT_map_buffer_range, GL_EXT_memory_object, GL_EXT_memory_object_fd,
+*/
